3f0ddbb8a460e2258602d21ece33fa0f31617d16
xescugc <xescugil@gmail.com>
2021-11-23 15:53:33 +0100

    prvider.go: Added also the GoogleProvider
    To the list of default Providers

--- provider.go
+++ provider.go
@@ -2,12 +2,14 @@ package terracost
 
 import (
 	"github.com/cycloidio/terracost/aws"
+	"github.com/cycloidio/terracost/google"
 	"github.com/cycloidio/terracost/terraform"
 )
 
 // defaultProviders are the currently known and supported terraform providers
 var defaultProviders = []terraform.ProviderInitializer{
 	aws.TerraformProviderInitializer,
+	google.TerraformProviderInitializer,
 }
 
 // getDefaultProviders will return the default supported providers of terracost
