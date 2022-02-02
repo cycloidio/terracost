1679ccea421a92e149dfdfa48aaef5a803301303
talset <talset55@gmail.com>
2022-02-02 18:06:36 +0100

    aws/services: Add ElastiCache

--- aws/services.go
+++ aws/services.go
@@ -2,9 +2,10 @@ package aws
 
 // SupportedServices is a list of all AWS services that are supported by Terracost.
 var supportedServices = map[string]struct{}{
-	"AmazonEC2": {},
-	"AmazonRDS": {},
-	"AWSELB":    {},
+	"AmazonEC2":         {},
+	"AmazonRDS":         {},
+	"AWSELB":            {},
+	"AmazonElastiCache": {},
 }
 
 // IsServiceSupported returns true if the AWS service is valid and supported by Terracost (e.g. for ingestion.)
