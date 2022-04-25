38d6aa60bcb32478f9b87e57b6c008eab1ee2eb2
talset <talset55@gmail.com>
2022-04-25 13:20:49 +0200

    examples: Provide golang sample with terracost
    Provide Samples/Example to help user to test Terracost

---
+++ examples/README.md
@@ -0,0 +1,62 @@
+# TerraCost samples
+
+Samples are only example to help you understand how to test Terracost.
+
+## Requirements
+
+### Mysql
+
+Cloud Provider pricing datas need to be ingested in a Mysql server. For testing purpose, local docker can be used
+
+```
+docker run  -p 3306:3306 -d --privileged  -e MYSQL_ROOT_PASSWORD=password  mysql:8.0.21 --default-authentication-plugin=mysql_native_password
+
+# Once Mysql started, create the terracost database
+mysql -h 127.0.0.1 -uroot -ppassword -e "CREATE DATABASE terracost"
+```
+
+## Examples
+
+### Pricing ingestion
+
+To start prices ingestion you need to first decide which cloud provider to ingest. In the example `AWS` is available with `-ingest-aws`.
+
+ and services
+
+ in `terracost-ingester.go`
+
+```
+go run terracost.go -ingest-aws
+```
+
+Here we ingest all supported services by Terracost, ingestion could takes severals minutes depending the amonts of datas.
+If you don't need all services, the list could be defined to reduce time.
+
+> Note
+> Before to ingest datas, the database migrations are needed. In order to correctly run migrations, we need `?multiStatements=true` param on DB connector
+> ```
+> sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/terracost?multiStatements=true")
+> ```
+
+### Pricing Estimation (from Plan)
+
+To estimate a terraform plan, you need to first generate it as json format
+
+```
+terraform init
+terraform plan -out update.tfplan
+terraform show -json update.tfplan > tfplan.json
+```
+
+Then run the estimation by specifying your json plan file path.
+
+```
+go run terracost.go -estimate-plan ./terraform-plan.json
+```
+
+### Pricing Estimation (from HCL)
+
+To estimate terraform code, define the terraform provider to use and the path of your terraform hcl code.
+```
+go run terracost.go -estimate-hcl-provider aws -estimate-hcl ../testdata/aws/stack-aws
+```
---
+++ examples/terracost.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cycloidio/terracost"
+	"github.com/cycloidio/terracost/aws"
+	"github.com/cycloidio/terracost/aws/region"
+	awstf "github.com/cycloidio/terracost/aws/terraform"
+	"github.com/cycloidio/terracost/cost"
+	"github.com/cycloidio/terracost/mysql"
+	"github.com/cycloidio/terracost/terraform"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func usage() {
+	fmt.Fprint(os.Stderr, "Terracost\n\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func awsIngest(region string, backend *mysql.Backend, db *sql.DB) {
+	fmt.Printf("AWS Ingestion")
+
+	// Run all database migrations
+	err := mysql.Migrate(context.Background(), db, "pricing_migrations")
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	// Ingest supported services pricing data into the database
+	for _, s := range aws.GetSupportedServices() {
+		fmt.Printf("[%s] Ingestion\n", s)
+		ingester, err := aws.NewIngester(s, region)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+
+		err = terracost.IngestPricing(context.Background(), backend, ingester)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func estimatePlan(path string, backend *mysql.Backend) {
+	fmt.Printf("EstimateTerraformPlan\n")
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	plan, err := terracost.EstimateTerraformPlan(context.Background(), backend, file)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	estimateDisplay(plan)
+}
+
+func estimateHCL(path string, provider string, backend *mysql.Backend) {
+	fmt.Printf("EstimateHCL %s\n", provider)
+
+	// From HCL
+	// Provider to use
+	var terraformAWSProviderInitializer = terraform.ProviderInitializer{
+		MatchNames: []string{provider, fmt.Sprintf("registry.terraform.io/hashicorp/%s", provider)},
+		Provider: func(config map[string]string) (terraform.Provider, error) {
+			r, ok := config["region"]
+			if !ok {
+				return nil, nil
+			}
+			regCode := region.Code(r)
+			return awstf.NewProvider(provider, regCode)
+		},
+	}
+
+	// terraform HCL directory
+	planhcl, err := terracost.EstimateHCL(context.Background(), backend, nil, path, terraformAWSProviderInitializer)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	estimateDisplay(planhcl)
+}
+
+func estimateDisplay(resourceDiff *cost.Plan) {
+	for _, res := range resourceDiff.ResourceDifferences() {
+		priorCost, err := res.PriorCost()
+		if err != nil {
+			fmt.Printf("PriorCost %s: %s", res.Address, err)
+			continue
+		}
+
+		plannedCost, err := res.PlannedCost()
+		if err != nil {
+			fmt.Printf("PlannedCost %s: %s", res.Address, err)
+			continue
+		}
+		fmt.Printf("%s: %s -> %s\n", res.Address, priorCost, plannedCost)
+	}
+}
+
+func main() {
+
+	var (
+		flagAWSIngest           bool   = false
+		flagAWSIngestRegion     string = "eu-west-1"
+		flagestimatePlan        string = ""
+		flagestimateHCL         string = ""
+		flagestimateHCLProvider string = "aws"
+	)
+
+	flag.Usage = usage
+	flag.BoolVar(&flagAWSIngest, "ingest-aws", flagAWSIngest, "Run AWS price ingester")
+	flag.StringVar(&flagAWSIngestRegion, "ingest-aws-region", flagAWSIngestRegion, "AWS region used to ingest")
+	flag.StringVar(&flagestimatePlan, "estimate-plan", flagestimatePlan, "terraform-plan.json file path to estimate (example: ./terraform-plan.json)")
+	flag.StringVar(&flagestimateHCL, "estimate-hcl", flagestimateHCL, "terraform HCL code path to estimate (example: ../testdata/aws/stack-aws)")
+	flag.StringVar(&flagestimateHCLProvider, "estimate-hcl-provider", flagestimateHCLProvider, "Terraform provider used")
+
+	flag.Parse()
+
+	// get command line args
+	args := flag.Args()
+	if len(args) == 0 {
+	}
+
+	if !flagAWSIngest && flagestimatePlan == "" && flagestimateHCL == "" {
+		usage()
+		os.Exit(0)
+	}
+
+	// Use your mysql access with MultiStatements
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/terracost?multiStatements=true")
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	backend := mysql.NewBackend(db)
+
+	if flagAWSIngest {
+		awsIngest(flagAWSIngestRegion, backend, db)
+	}
+
+	if flagestimatePlan != "" {
+		estimatePlan(flagestimatePlan, backend)
+	}
+
+	if flagestimateHCL != "" {
+		estimateHCL(flagestimateHCL, flagestimateHCLProvider, backend)
+	}
+
+}
